0275f725b591b29ec88242b47f976077a1cd79f1
Andelf <andelf@gmail.com>
2024-10-04 09:03:04 +0800

    feat(bmp180): async driver

---
+++ bmp180/src/blocking.rs
@@ -0,0 +1,117 @@
+use embedded_hal::delay::DelayNs;
+use micromath::F32Ext;
+
+use super::{cmds, regs, CalibrationData, Config, Error, Measurement, Mode, ADDRESS};
+
+/// BMP180, or BMP085.
+pub struct BMP180<I2C: embedded_hal::i2c::I2c> {
+    i2c: I2C,
+    addr: u8,
+    // oversampling settings
+    mode: Mode,
+    calib: CalibrationData,
+}
+
+impl<I2C: embedded_hal::i2c::I2c> BMP180<I2C> {
+    /// Create device driver instance.
+    pub fn new(i2c: I2C, addr: u8) -> Self {
+        BMP180 {
+            i2c,
+            addr,
+            mode: Mode::UltraHighResolution,
+            calib: unsafe { core::mem::zeroed() },
+        }
+    }
+
+    /// Create a new instance of BMP180 with the default address.
+    pub fn new_primary(i2c: I2C) -> Self {
+        BMP180::new(i2c, ADDRESS)
+    }
+
+    /// Read calibration data from the EEPROM of BMP180.
+    pub fn init(&mut self, config: Config) -> Result<(), Error<I2C::Error>> {
+        let mut buf = [0u8; 22];
+        self.i2c.write_read(self.addr, &[regs::CAL_AC1], &mut buf[..])?;
+
+        self.calib = CalibrationData::from_raw(&buf);
+
+        self.mode = config.mode;
+
+        Ok(())
+    }
+
+    /// read uncompensated temperature value
+    #[inline]
+    fn read_ut<D: DelayNs>(&mut self, delay: &mut D) -> Result<i32, Error<I2C::Error>> {
+        self.write_reg(regs::CONTROL, cmds::READTEMPCMD)?;
+        delay.delay_ms(5);
+
+        let mut buf = [0u8; 2];
+        self.i2c.write_read(self.addr, &[regs::TEMPDATA], &mut buf)?;
+
+        Ok(((buf[0] as i32) << 8) + (buf[1] as i32))
+    }
+
+    /// read uncompensated pressure value
+    #[inline]
+    fn read_up<D: DelayNs>(&mut self, delay: &mut D) -> Result<i32, Error<I2C::Error>> {
+        let oss = self.mode.oversampling_settings();
+        self.write_reg(regs::CONTROL, cmds::READPRESSURECMD + (oss << 6))?;
+        delay.delay_ms(self.mode.conversion_delay_in_ms() as u32);
+
+        let mut buf = [0u8; 3];
+        self.i2c.write_read(self.addr, &[regs::PRESSUREDATA], &mut buf)?;
+
+        let up = ((buf[0] as i32) << 16) + ((buf[1] as i32) << 8) + (buf[2] as i32) >> (8 - oss);
+
+        Ok(up)
+    }
+
+    /// Calculate true temperature, resolution is 0.1C
+    pub fn read_temperature<D: DelayNs>(&mut self, delay: &mut D) -> Result<f32, Error<I2C::Error>> {
+        let ut = self.read_ut(delay)?;
+
+        Ok(super::convert_temperature(ut, &self.calib))
+    }
+
+    /// Read temperature and pressure at once
+    pub fn read_measurement<D: DelayNs>(&mut self, delay: &mut D) -> Result<Measurement, Error<I2C::Error>> {
+        let ut = self.read_ut(delay)?;
+        let up = self.read_up(delay)?;
+
+        let measure = super::convert_measurement(up, ut, &self.calib, self.mode);
+
+        Ok(measure)
+    }
+
+    /// Calculate true pressure, in Pa
+    pub fn read_pressure<D: DelayNs>(&mut self, delay: &mut D) -> Result<i32, Error<I2C::Error>> {
+        self.read_measurement(delay).map(|m| m.pressure)
+    }
+
+    /// Calculate absolute altitude
+    pub fn calculate_altitude<D: DelayNs>(
+        &mut self,
+        delay: &mut D,
+        sealevel_pressure: f32,
+    ) -> Result<f32, Error<I2C::Error>> {
+        let pa = self.read_pressure(delay)? as f32;
+        Ok(44330.0 * (1.0 - (pa / sealevel_pressure).powf(1.0 / 5.255)))
+    }
+
+    /// Calculate pressure at sea level
+    pub fn calculate_sealevel_pressure<D: DelayNs>(
+        &mut self,
+        delay: &mut D,
+        altitude_m: f32,
+    ) -> Result<u32, Error<I2C::Error>> {
+        let pressure = self.read_pressure(delay)? as f32;
+        let p0 = pressure / (1.0 - altitude_m / 44330.0).powf(5.255);
+        Ok(p0 as u32)
+    }
+
+    fn write_reg(&mut self, reg: u8, value: u8) -> Result<(), Error<I2C::Error>> {
+        self.i2c.write(self.addr, &[reg, value])?;
+        Ok(())
+    }
+}
--- bmp180/src/lib.rs
+++ bmp180/src/lib.rs
@@ -2,9 +2,10 @@
 
 #![no_std]
 
-use embedded_hal::delay::DelayNs;
-use embedded_hal::i2c::I2c;
+use embedded_hal_async::delay::DelayNs;
 use micromath::F32Ext;
+
+pub mod blocking;
 
 // BMP180, BMP085 address.
 pub const ADDRESS: u8 = 0x77;
@@ -60,14 +61,13 @@ impl Mode {
         *self as u8
     }
 
-    fn wait_conversion<D: DelayNs>(&self, delay: &mut D) {
-        let ms = match self {
+    fn conversion_delay_in_ms(&self) -> u32 {
+        match self {
             Mode::UltraLowPower => 5,
             Mode::Standard => 8,
             Mode::HighResolution => 14,
             Mode::UltraHighResolution => 26,
-        };
-        delay.delay_ms(ms);
+        }
     }
 }
 
@@ -126,7 +126,7 @@ pub struct Measurement {
 }
 
 /// BMP180, or BMP085.
-pub struct BMP180<I2C> {
+pub struct BMP180<I2C: embedded_hal_async::i2c::I2c> {
     i2c: I2C,
     addr: u8,
     // oversampling settings
@@ -134,7 +134,7 @@ pub struct BMP180<I2C> {
     calib: CalibrationData,
 }
 
-impl<I2C: I2c> BMP180<I2C> {
+impl<I2C: embedded_hal_async::i2c::I2c> BMP180<I2C> {
     /// Create device driver instance.
     pub fn new(i2c: I2C, addr: u8) -> Self {
         BMP180 {
@@ -151,9 +151,9 @@ impl<I2C: I2c> BMP180<I2C> {
     }
 
     /// Read calibration data from the EEPROM of BMP180.
-    pub fn init(&mut self, config: Config) -> Result<(), Error<I2C::Error>> {
+    pub async fn init(&mut self, config: Config) -> Result<(), Error<I2C::Error>> {
         let mut buf = [0u8; 22];
-        self.i2c.write_read(self.addr, &[regs::CAL_AC1], &mut buf[..])?;
+        self.i2c.write_read(self.addr, &[regs::CAL_AC1], &mut buf[..]).await?;
 
         self.calib = CalibrationData::from_raw(&buf);
 
@@ -164,25 +164,26 @@ impl<I2C: I2c> BMP180<I2C> {
 
     /// read uncompensated temperature value
     #[inline]
-    fn read_ut<D: DelayNs>(&mut self, delay: &mut D) -> Result<i32, Error<I2C::Error>> {
-        self.write(&[regs::CONTROL, cmds::READTEMPCMD])?;
-        delay.delay_ms(5);
+    async fn read_ut<D: DelayNs>(&mut self, delay: &mut D) -> Result<i32, Error<I2C::Error>> {
+        self.write_reg(regs::CONTROL, cmds::READTEMPCMD).await?;
+        delay.delay_ms(5).await;
 
         let mut buf = [0u8; 2];
-        self.i2c.write_read(self.addr, &[regs::TEMPDATA], &mut buf)?;
+        self.i2c.write_read(self.addr, &[regs::TEMPDATA], &mut buf).await?;
 
         Ok(((buf[0] as i32) << 8) + (buf[1] as i32))
     }
 
     /// read uncompensated pressure value
     #[inline]
-    fn read_up<D: DelayNs>(&mut self, delay: &mut D) -> Result<i32, Error<I2C::Error>> {
+    async fn read_up<D: DelayNs>(&mut self, delay: &mut D) -> Result<i32, Error<I2C::Error>> {
         let oss = self.mode.oversampling_settings();
-        self.write(&[regs::CONTROL, cmds::READPRESSURECMD + (oss << 6)])?;
-        self.mode.wait_conversion(delay);
+        self.write_reg(regs::CONTROL, cmds::READPRESSURECMD + (oss << 6))
+            .await?;
+        delay.delay_ms(self.mode.conversion_delay_in_ms() as u32).await;
 
         let mut buf = [0u8; 3];
-        self.i2c.write_read(self.addr, &[regs::PRESSUREDATA], &mut buf)?;
+        self.i2c.write_read(self.addr, &[regs::PRESSUREDATA], &mut buf).await?;
 
         let up = ((buf[0] as i32) << 16) + ((buf[1] as i32) << 8) + (buf[2] as i32) >> (8 - oss);
 
@@ -190,86 +191,96 @@ impl<I2C: I2c> BMP180<I2C> {
     }
 
     /// Calculate true temperature, resolution is 0.1C
-    pub fn read_temperature<D: DelayNs>(&mut self, delay: &mut D) -> Result<f32, Error<I2C::Error>> {
-        let ut = self.read_ut(delay)?;
+    pub async fn read_temperature<D: DelayNs>(&mut self, delay: &mut D) -> Result<f32, Error<I2C::Error>> {
+        let ut = self.read_ut(delay).await?;
 
-        let x1 = ((ut - self.calib.ac6 as i32) * self.calib.ac5 as i32) >> 15;
-        let x2 = ((self.calib.mc as i32) << 11) / (x1 + self.calib.md as i32);
-        let b5 = x1 + x2;
-        Ok(((b5 + 8) >> 4) as f32 / 10.0)
+        Ok(convert_temperature(ut, &self.calib))
     }
 
     /// Read temperature and pressure at once
-    pub fn read_measurement<D: DelayNs>(&mut self, delay: &mut D) -> Result<Measurement, Error<I2C::Error>> {
-        let oss = self.mode.oversampling_settings();
+    pub async fn read_measurement<D: DelayNs>(&mut self, delay: &mut D) -> Result<Measurement, Error<I2C::Error>> {
+        let ut = self.read_ut(delay).await?;
+        let up = self.read_up(delay).await?;
 
-        let ut = self.read_ut(delay)?;
-        let up = self.read_up(delay)?;
-
-        let x1 = ((ut - self.calib.ac6 as i32) * self.calib.ac5 as i32) >> 15;
-        let x2 = ((self.calib.mc as i32) << 11) / (x1 + self.calib.md as i32);
-        let b5 = x1 + x2;
-        let t = ((b5 + 8) >> 4) as f32 / 10.0;
-
-        let b6 = b5 - 4000;
-        let x1 = ((self.calib.b2 as i32) * ((b6 * b6) >> 12)) >> 11;
-        let x2 = ((self.calib.ac2 as i32) * b6) >> 11;
-        let x3 = x1 + x2;
-        // NOTE: must use i64 type
-        let b3: i64 = ((((self.calib.ac1 as i64) * 4 + x3 as i64) << oss) + 2) / 4;
-
-        let x1 = ((self.calib.ac3 as i32) * b6) >> 13;
-        let x2 = ((self.calib.b1 as i32) * ((b6 * b6) >> 12)) >> 16;
-        let x3 = ((x1 + x2) + 2) >> 2;
-        let b4: u32 = (self.calib.ac4 as u32) * ((x3 + 32768) as u32) >> 15;
-
-        let b7 = (up as i64 - b3 as i64) * (50000 >> oss);
-
-        let p = if b7 < 0x80000000 {
-            (b7 * 2) / (b4 as i64)
-        } else {
-            (b7 / (b4 as i64)) * 2
-        };
-
-        let x1 = (p >> 8) * (p >> 8);
-        let x1 = (x1 * 3038) >> 16;
-        let x2 = (-7357 * p) >> 16;
-        let p = p + ((x1 + x2 + 3791) >> 4);
-
-        Ok(Measurement {
-            temperature: t,
-            pressure: p as i32,
-        })
+        Ok(convert_measurement(up, ut, &self.calib, self.mode))
     }
 
     /// Calculate true pressure, in Pa
-    pub fn read_pressure<D: DelayNs>(&mut self, delay: &mut D) -> Result<i32, Error<I2C::Error>> {
-        self.read_measurement(delay).map(|m| m.pressure)
+    pub async fn read_pressure<D: DelayNs>(&mut self, delay: &mut D) -> Result<i32, Error<I2C::Error>> {
+        self.read_measurement(delay).await.map(|m| m.pressure)
     }
 
     /// Calculate absolute altitude
-    pub fn calculate_altitude<D: DelayNs>(
+    pub async fn calculate_altitude<D: DelayNs>(
         &mut self,
         delay: &mut D,
         sealevel_pressure: f32,
     ) -> Result<f32, Error<I2C::Error>> {
-        let pa = self.read_pressure(delay)? as f32;
+        let pa = self.read_pressure(delay).await? as f32;
         Ok(44330.0 * (1.0 - (pa / sealevel_pressure).powf(1.0 / 5.255)))
     }
 
     /// Calculate pressure at sea level
-    pub fn calculate_sealevel_pressure<D: DelayNs>(
+    pub async fn calculate_sealevel_pressure<D: DelayNs>(
         &mut self,
         delay: &mut D,
         altitude_m: f32,
     ) -> Result<u32, Error<I2C::Error>> {
-        let pressure = self.read_pressure(delay)? as f32;
+        let pressure = self.read_pressure(delay).await? as f32;
         let p0 = pressure / (1.0 - altitude_m / 44330.0).powf(5.255);
         Ok(p0 as u32)
     }
 
-    fn write(&mut self, data: &[u8]) -> Result<(), Error<I2C::Error>> {
-        self.i2c.write(self.addr, data)?;
+    async fn write_reg(&mut self, reg: u8, value: u8) -> Result<(), Error<I2C::Error>> {
+        self.i2c.write(self.addr, &[reg, value]).await?;
         Ok(())
+    }
+}
+
+#[inline]
+fn convert_temperature(ut: i32, calib: &CalibrationData) -> f32 {
+    let x1 = ((ut - calib.ac6 as i32) * calib.ac5 as i32) >> 15;
+    let x2 = ((calib.mc as i32) << 11) / (x1 + calib.md as i32);
+    let b5 = x1 + x2;
+    ((b5 + 8) >> 4) as f32 / 10.0
+}
+
+#[inline]
+fn convert_measurement(up: i32, ut: i32, calib: &CalibrationData, mode: Mode) -> Measurement {
+    let oss = mode.oversampling_settings();
+
+    let x1 = ((ut - calib.ac6 as i32) * calib.ac5 as i32) >> 15;
+    let x2 = ((calib.mc as i32) << 11) / (x1 + calib.md as i32);
+    let b5 = x1 + x2;
+    let t = ((b5 + 8) >> 4) as f32 / 10.0;
+
+    let b6 = b5 - 4000;
+    let x1 = ((calib.b2 as i32) * ((b6 * b6) >> 12)) >> 11;
+    let x2 = ((calib.ac2 as i32) * b6) >> 11;
+    let x3 = x1 + x2;
+    // NOTE: must use i64 type
+    let b3: i64 = ((((calib.ac1 as i64) * 4 + x3 as i64) << oss) + 2) / 4;
+
+    let x1 = ((calib.ac3 as i32) * b6) >> 13;
+    let x2 = ((calib.b1 as i32) * ((b6 * b6) >> 12)) >> 16;
+    let x3 = ((x1 + x2) + 2) >> 2;
+    let b4: u32 = (calib.ac4 as u32) * ((x3 + 32768) as u32) >> 15;
+
+    let b7 = (up as i64 - b3 as i64) * (50000 >> oss);
+
+    let p = if b7 < 0x80000000 {
+        (b7 * 2) / (b4 as i64)
+    } else {
+        (b7 / (b4 as i64)) * 2
+    };
+
+    let x1 = (p >> 8) * (p >> 8);
+    let x1 = (x1 * 3038) >> 16;
+    let x2 = (-7357 * p) >> 16;
+    let p = p + ((x1 + x2 + 3791) >> 4);
+
+    Measurement {
+        temperature: t,
+        pressure: p as i32,
     }
 }
